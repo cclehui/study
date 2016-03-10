a284c86429ca0ce71d0c91411e06316dd0d8d141
cclehui <god_chen@sina.com>
2016-03-10 17:43:16 +0800

    Php socket 编程demo

---
+++ socket/socket_php_client.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *  
+ * socket 客户端程序  
+ *  
+ */
+
+$service_port = 10006;
+$address = '127.0.0.1';
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false) {
+    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
+}
+
+$result = socket_connect($socket, $address, $service_port);
+//var_dump(socket_set_nonblock($socket));
+if($result === false) {
+    echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+}
+
+echo "connected source $result\n";
+
+$count = 1;
+
+
+do{
+    $out = "";
+
+
+
+    //$in = "test string " . date("Y-m-d H:i:s") ;
+
+    echo "please input you data:\n";
+    $console_input = trim(fgets(STDIN), "\r\n");
+    //$console_input .= "\t" . time();
+
+    //echo "aaaa:", $console_input, "\n";
+
+    socket_write($socket, $console_input, strlen($console_input));
+
+    $out = socket_read($socket, 8192);
+    echo $count, "\t", $out, "\n";
+
+    $out = $out ? $out : 1;
+    //sleep(2);
+
+    $count++;
+
+} while ($out);
+
+socket_close($socket);
---
+++ socket/socket_php_server.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * socket 服务端程序  
+ *  
+ */
+
+error_reporting(E_ALL);
+set_time_limit(0);
+//ob_implicit_flush();
+
+$address = '127.0.0.1';
+$port = 10006;
+//创建端口
+if( ($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+    echo "socket_create() failed :reason:" . socket_strerror(socket_last_error()) . "\n";
+}
+
+//绑定
+if (socket_bind($sock, $address, $port) === false) {
+    echo "socket_bind() failed :reason:" . socket_strerror(socket_last_error($sock)) . "\n";
+}
+
+//监听
+if (socket_listen($sock, 5) === false) {
+    echo "socket_bind() failed :reason:" . socket_strerror(socket_last_error($sock)) . "\n";
+}
+
+//socket_set_nonblock($sock);
+
+$read_array = array($sock);
+$write_array = array();
+
+$connected_socket = array();
+$result_array = array();
+
+//开始处理数据和连接
+while (1) {
+    $read_array = array_merge($connected_socket, array($sock));
+    $write_array = $result_array ? $connected_socket : null;
+    //$exception_array = array(STDOUT);
+    $exception_array = null;
+
+    $selected_count = socket_select($read_array, $write_array, $exception_array, 3);
+
+    //sleep(1);
+    print_r($connected_socket);
+    print_r($read_array);
+    print_r($write_array);
+    print_r($result_array);
+    echo "\ncount:", $selected_count, "\n";
+
+    if ($selected_count) {
+
+        //处理读
+        if ($read_array) {
+            foreach ($read_array as $item) {
+
+                if ($item == $sock) {
+
+                    $new_connect = socket_accept($item);
+                    if ($new_connect) {
+                        socket_set_nonblock($new_connect);//新链接设置为非阻塞的
+                        $connected_socket[] = $new_connect;
+                    }
+
+                } else {
+                    $temp_result = doRead($item);
+                    $index = array_search($item, $connected_socket);
+
+                    if ($temp_result === false) {
+                        socket_close($item);
+                        unset($connected_socket[$index]);
+                    } elseif ($temp_result) {
+                        $result_array[$index] = $temp_result;
+                    }
+                }
+            }
+        }
+
+        //处理写
+        if ($write_array) {
+            foreach ($write_array as $item) {
+                $index = array_search($item, $connected_socket);
+
+                if ($index !== false && isset($result_array[$index]) && $result_array[$index]) {
+                    if ( false === socket_write($item, $result_array[$index], strlen($result_array[$index]))) {
+                        socket_close($item);
+                    }
+
+                    unset($result_array[$index]);
+                }
+            }
+        }
+    }
+}
+
+//关闭socket
+socket_close($sock);
+
+/******************************/
+function doRead($connected_socket) {
+
+    $buffer = "";
+
+    do {
+
+        $temp_buf = socket_read($connected_socket, 8192);
+
+        $buffer .= $temp_buf;
+
+    } while ($temp_buf);
+
+    if ($temp_buf === "") {
+        $str = "\n";
+        socket_write($connected_socket, $str, strlen($str));
+        $error_code = socket_last_error($connected_socket);
+        //var_dump($error_code);
+        //echo socket_strerror($error_code), "\n";
+        //echo $error_code, "\txxxxxxxxxx\n";
+        if (in_array($error_code, array(11, 32))) {//链接被关闭
+            return false;
+        }
+    }
+
+    $result = $buffer ? doAction($buffer) : "";
+
+    return $result;
+
+}
+
+function doAction($str) {
+    return strrev($str);
+}
+
+
+
+?>
+/**
+ *  
+ */

d3d1ebb2ecf8f48b890dda95bc191143f9eb032c
cclehui <cclehui>
2019-02-27 19:58:23 +0800

    heap sort

--- datastructure/php/Sort/HeapSort.php
+++ datastructure/php/Sort/HeapSort.php
@@ -4,6 +4,7 @@ include "./Sort.php";
 //namespace Cclehui\Study\Sort
 
 
+//https://blog.csdn.net/u013384984/article/details/79496052
 class HeapSort extends Sort {
 
     public function sort($data_set, $start_index = null, $end_index = null) {
@@ -11,13 +12,50 @@ class HeapSort extends Sort {
             return $data_set;
         }
 
-        for ($i=1;$i<=count($data_set);$i++) {
-            
+        $data_length = count($data_set);
+
+        for ($i= intval($data_length / 2) - 1  ;$i>=0;$i--) {
+            $this->adjustHeap($data_set, $i, $data_length);
         }
 
-        return $result;
+        for ($i=0; $i<$data_length; $i++) {
+            $j = $data_length - 1 - $i;
+            if ($j == 0) {
+                break;
+            }
+
+            $data_set[0] += $data_set[$j];
+            $data_set[$j] = $data_set[0] - $data_set[$j];
+            $data_set[0] = $data_set[0] - $data_set[$j];
+
+
+            $this->adjustHeap($data_set, 0, $j);
+        }
+
+        return $data_set;
     }
-    
+
+    protected function adjustHeap(&$data_set, $i, $data_length) {
+
+        
+        for ($j = $i * 2; $j < $data_length ; ) {
+
+            if ($j + 1 < $data_length) {
+                if ($data_set[$j] < $data_set[$j + 1]) {
+                    $j++;
+                }
+            }
+
+            if ($data_set[$j] > $data_set[$i]) {
+                $data_set[$i] += $data_set[$j];
+                $data_set[$j] = $data_set[$i] - $data_set[$j];
+                $data_set[$i] = $data_set[$i] - $data_set[$j];
+            } else {
+                break;
+            }
+        }
+        
+    }
 }
 
 
